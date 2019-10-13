dcf4446a1290babef1f360763c3b9c1519af273b
Unknown <alvinquach91@gmail.com>
2019-10-12 19:45:09 -0700

    added CORS headers

--- src/loaders/express.loader.ts
+++ src/loaders/express.loader.ts
@@ -1,6 +1,15 @@
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 
 export default (app: Application) => {
     app.set('port', process.env.PORT || 3000);
     app.use(express.json());
+
+    // CORS
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        next();
+    });
 };

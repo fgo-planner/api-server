1d483931406c9aea073658ac55ce03f0ac55f4a9
Unknown <alvinquach91@gmail.com>
2019-10-19 15:30:24 -0700

    added security to game items controller

--- src/controllers/game-item.controller.ts
+++ src/controllers/game-item.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { Inject } from 'typedi';
-import { GetMapping, PostMapping, PutMapping, RestController } from '../internal/decorators/rest-controller.decorator';
+import { RouteSecurityLevel } from '../internal';
+import { GetMapping, PostMapping, PutMapping, RestController, Secured } from '../internal/';
 import { GameItemService } from '../services/game/object/game-item.service';
 
 @RestController('/game-item')
@@ -9,9 +10,9 @@ export class GameItemController {
     @Inject()
     private _gameItemService: GameItemService;
 
-    // TODO Make this admin only
     @PutMapping()
-    createItem(req: Request, res: Response) {
+    @Secured(RouteSecurityLevel.ADMIN)
+    createGameItem(req: Request, res: Response) {
         const item = req.body;
         this._gameItemService.createItem(item).then(
             created => res.send(created),
@@ -20,7 +21,8 @@ export class GameItemController {
     }
 
     @GetMapping()
-    findItems(req: Request, res: Response) {
+    @Secured(RouteSecurityLevel.ADMIN)
+    findGameItems(req: Request, res: Response) {
         this._gameItemService.findItems().then(
             items => res.send(items),
             err => res.status(404).send(err)
@@ -28,7 +30,8 @@ export class GameItemController {
     }
 
     @PostMapping()
-    updateitem(req: Request, res: Response) {
+    @Secured(RouteSecurityLevel.ADMIN)
+    updateGameItem(req: Request, res: Response) {
         const item = req.body;
         this._gameItemService.updateItem(item).then(
             updated => res.send(updated),

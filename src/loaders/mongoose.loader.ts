bd55265e105c33a9a3ce7d569ae48811b15ccb2a
Unknown <alvinquach91@gmail.com>
2020-04-09 13:48:31 -0700

    fixed mongoose deprecation warning and type errors

--- src/loaders/mongoose.loader.ts
+++ src/loaders/mongoose.loader.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
 export default () => {
+    mongoose.set('useFindAndModify', false);
+
     mongoose.connection.on('connected', () => console.log(`Mongoose connected to ${process.env.MONGODB_URI}`));
     mongoose.connection.on('disconnected', () => console.log('Mongoose disconnected.'));
     mongoose.connect(process.env.MONGODB_URI);

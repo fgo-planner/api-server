8faf8eddb6e7fde8856e573e50f26ab65ed72c6f
Alvin Quach <alvinquach91@gmail.com>
2022-01-29 14:18:04 -0800

    added docker scripts

---
+++ docker-build.sh
@@ -0,0 +1,10 @@
+# Set permission to 777 (chmod 777) in order to execute this file.
+
+# Stop current container if its running.
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=fgo-planner-api-server --format="{{.ID}}"))
+
+# Remove previous image if it exists.
+docker image rm fgo-planner-api-server
+
+# Build new image.
+docker build . -t fgo-planner-api-server
---
+++ docker-run.sh
@@ -0,0 +1,3 @@
+# Set permission to 777 (chmod 777) in order to execute this file.
+
+docker run -d -p 8080:8080 -t fgo-planner-api-server
